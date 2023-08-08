2f10d92cba8649f2b23fda1a6e3e0a66f775f3db
waranlogesh123 <141731044+waranlogesh123@users.noreply.github.com>
2023-08-08 16:27:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const changeColorButton = document.getElementById('changeColorButton');
-const mainContent = document.querySelector('main');
-
-changeColorButton.addEventListener('click', () => {
-    mainContent.style.color = getRandomColor();
-});
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+const reviveEntries = () => {
+    let entries = localStorage.getItem("user-Entries");
+    return entries ? JSON.parse(entries) : [];
+  };
+  
+  let userEntries = reviveEntries();
+  
+  const printEntries = () => {
+    const entries = reviveEntries();
+  
+    const tableEntries = entries
+        .map(
+            (entry) => `<tr>
+                <td style = "padding: 2px 4px">${entry.name}</td>
+                <td style = "padding: 2px 4px">${entry.email}</td>
+                <td style = "padding: 2px 4px">${entry.password}</td>
+                <td style = "padding: 2px 4px">${entry.dob}</td>
+                <td style = "padding: 2px 4px">${entry.acceptedTermsAndConditions}</td>
+            </tr>`
+        )
+        .join("\n");
+  
+    const table = `<table>
+        
+        <tbody>${tableEntries}</tbody>
+    </table>`;
+  
+    let details = document.getElementById("user-entries");
+    details.innerHTML = table;
+  };
+  
+  const uploadForm = (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTermsAndConditions = document.getElementById("Accepted term").checked;
+  
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions,
+    };
+  
+    userEntries.push(entry);
+    localStorage.setItem("user-Entries", JSON.stringify(userEntries));
+    printEntries();
+    userForm.reset();
+  };
+  
+  let userForm = document.getElementById("form");
+  userForm.addEventListener("submit", uploadForm);
+  
+  printEntries();
